05db0d4645c25d543c85a89290d6b611d9eb1fb4
zeeny <157370421+kzeen@users.noreply.github.com>
2025-03-02 22:04:22 +0200

    Add onClickListener for egg image

--- 1-egg-counter/app/src/main/java/com/example/eggcounter/MainActivity.java
+++ 1-egg-counter/app/src/main/java/com/example/eggcounter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.eggcounter;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv_egg;
+    private TextView tv_counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initViews();
+        addListeners();
+    }
+
+    private void initViews() {
+        iv_egg = findViewById(R.id.iv_egg);
+        tv_counter = findViewById(R.id.tv_counter);
+    }
+
+    private void addListeners() {
+        iv_egg.setOnClickListener(v -> incrementCounter());
     }
 }
