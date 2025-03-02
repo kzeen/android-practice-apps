d847c370ec7ada88ab70b95e8bbbcad665f0b785
zeeny <157370421+kzeen@users.noreply.github.com>
2025-03-02 22:21:22 +0200

    Add onClickListener to reset button

--- 1-egg-counter/app/src/main/java/com/example/eggcounter/MainActivity.java
+++ 1-egg-counter/app/src/main/java/com/example/eggcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eggcounter;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private ImageView iv_egg;
     private TextView tv_counter;
+    private Button bt_reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         iv_egg = findViewById(R.id.iv_egg);
         tv_counter = findViewById(R.id.tv_counter);
+        bt_reset = findViewById(R.id.bt_reset);
     }
 
     private void addListeners() {
         iv_egg.setOnClickListener(v -> incrementCounter());
+        bt_reset.setOnClickListener(v -> resetCounter());
     }
 
     private void incrementCounter() {
